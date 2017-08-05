5ed4b10a6e53a82cce5c495eb77ed19fe7d0ea84
xufengxu <943138563@qq.com>
2017-08-05 19:42:17 +0800

    Update bubble_sort

--- projects/BasicAlgorithm/sorting_algorithm.py
+++ projects/BasicAlgorithm/sorting_algorithm.py
@@ -1,30 +1,47 @@
 """ 基本的排序算法 """
-order_list = [35, 28, 61, 78, 89, 50, 135]
+record_list = [35, 28, 61, 78, 89, 50, 135]
 
-list_2 = [2, 1, 3, 4, 5, 8, 6, 7]
+list_2 = [2, 1, 3, 4, 5, 6, 7]
 
 
 # 冒泡排序
-# 第0个，从1起，和之后的每个进行对比，大了则互换位置
-# 第1个，从2起， 和之后的每个对比 ……
-# 第i个， 从i+1起，……
-# i+1 <= length, 故 i 需 0 < i < length -1
+# 窗口为2移动，两两比较
 
-# (可进行优化)
-def bubble_sort(order_list):
+def bubble_sort(record_list):
     count = 0
-    length = len(order_list)
+    length = len(record_list)
 
     for i in range(length - 1):
-        for j in range(i + 1, length):
+        print('=======')
+
+        for j in range(length - i - 1):
             count += 1
 
-            if order_list[i] > order_list[j]:
-                order_list[j], order_list[i] = order_list[i], order_list[j]
-    print('count', count)
-    return order_list
+            if record_list[j] > record_list[j + 1]:
+                print(j, j + 1)
+
+                record_list[j], record_list[j + 1] = record_list[j + 1], record_list[j]
+    print('order_list', record_list, count)
 
 
-res = bubble_sort(list_2)
-print('res', res)
+bubble_sort(record_list)
 
+
+def bubble_sort_update(order_list):
+    """ 添加了 flag ，有数据交换，则在进行下一轮循环判断"""
+    count = 0
+    length = len(order_list)
+    flag = True
+    for i in range(length - 1):
+        if flag:
+            flag = False
+            for j in range(length - i - 1):
+                count += 1
+
+                if order_list[j] > order_list[j + 1]:
+                    order_list[j], order_list[j + 1] = order_list[j + 1], order_list[j]
+                    flag = True
+    print('order_list', order_list, count)
+
+
+bubble_sort_update(list_2)
