250322dc193d577a826879002110e643f775fc17
xufengxu <943138563@qq.com>
2017-11-29 11:48:59 +0800

    Update ensure_connection

--- kumbu_demo/f_s.py
+++ kumbu_demo/f_s.py
@@ -9,6 +9,7 @@ from kombu.mixins import ConsumerMixin
 from kumbu_demo import services
 
 import os
+
 filepath = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'log_conf.ini')
 
@@ -31,7 +32,7 @@ class Server(ConsumerMixin):
         )]
 
     def on_request(self, message):
-        LOGGER.info('message.payload: %s',  message.payload)
+        LOGGER.info('message.payload: %s', message.payload)
         fun = message.payload['fun']
         args = message.payload['args']
         kwargs = message.payload['kwargs']
@@ -39,11 +40,12 @@ class Server(ConsumerMixin):
         function = getattr(services, fun)
         result = function(*args, **kwargs)
 
-        LOGGER.info('on_request: %s',  result)
+        LOGGER.info('on_request: %s', result)
 
         self.producer.publish(
             {'result': result},
-            exchange='', routing_key=message.properties['reply_to'],
+            exchange='',
+            routing_key=message.properties['reply_to'],
             correlation_id=message.properties['correlation_id'],
             serializer='json',
             retry=True,
@@ -51,15 +53,31 @@ class Server(ConsumerMixin):
         message.ack()
 
 
-def start_worker(broker_url):
-    connection = Connection(broker_url)
-    LOGGER.info('Awaiting RPC requests')
-    worker = Server(connection)
-    worker.run()
+def get_server_config():
+    try:
+        from server.local_config import RPC_SERVER_CONFIG
+        config = RPC_SERVER_CONFIG
+    except ImportError:
+        config = {'host': 'localhost', 'port': 5672, 'virtual_host': '/', 'userid': 'guest', 'password': 'guest'}
+
+    return config
+
+
+def start_worker(config):
+    connection = Connection(**config)
+    try:
+        # 0、1、2 进行三次连接尝试
+        connection.ensure_connection(max_retries=2)
+        LOGGER.info('Awaiting RPC requests')
+        worker = Server(connection)
+        worker.run()
+    except OSError as error:
+        LOGGER.error('连接错误 : %s', error)
 
 
 if __name__ == '__main__':
     try:
-        start_worker('amqp://guest:guest@localhost:5672//')
+        config = get_server_config()
+        start_worker(config)
     except KeyboardInterrupt:
         pass
